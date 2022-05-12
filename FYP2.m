bc6dc57bfa54797cfb246220d1428e266f421a85
Zayan <particlegriffin@gmail.com>
2022-05-12 17:26:03 +0800

    Update: Added new file to work on

---
+++ FYP2.m
@@ -0,0 +1,12 @@
+clc;
+clear;
+
+UEd = 4; % 2 pairs of D2D devices
+UEc = 5; % 5 cellular devices
+
+dd = 10; % distance between d2d devices [m]
+dcb = 50; % distance between cellular and base-station [m]
+
+% power vector of all users
+pi = ones(UEd,1)*2.22e-16;
+pk = ones(UEc,1)*2.22e-16;
