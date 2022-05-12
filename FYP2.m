a66b18a6eeadb07cd543a5e0069c57e63450a9bf
Zayan <particlegriffin@gmail.com>
2022-05-12 22:21:38 +0800

    Added variables

--- FYP2.m
+++ FYP2.m
@@ -8,5 +8,6 @@ dd = 10; % distance between d2d devices [m]
 dcb = 50; % distance between cellular and base-station [m]
 
 % power vector of all users
-pi = ones(UEd,1)*2.22e-16;
-pk = ones(UEc,1)*2.22e-16;
+pi = ones(UEd,1)*2.22e-16; % power vector of D2D users [W]
+pk = ones(UEc,1)*2.22e-16; % power vector of Cellular users (fixed) [W]
+
