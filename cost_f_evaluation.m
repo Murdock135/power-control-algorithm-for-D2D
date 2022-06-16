60db324ccec9496dbd5246af41144c8897e1a5a4
Zayan <particlegriffin@gmail.com>
2022-06-16 10:46:27 +0800

    cost vs pi (no UEc) done

--- cost_f_evaluation.m
+++ cost_f_evaluation.m
@@ -1,5 +1,6 @@
 clc;
 clear;
+close all;
 
 UEd = 4; % 2 D2D devices (1 pair)
 UEc = 1; % 1 cellular devices
@@ -25,26 +26,61 @@ gik = 1.*(100./d_ik).^2; % link gain between d2d and cellular devices
 %%  algorithm parameters
 
 pmax = 1000e-3; % 1000 mW
-tau = 5; % target SIR is 5 
+tau = 05; % target SIR is 5 
 
 % sigmoid parameters
 b = 2;
 c = 10;
-%% Evaluation of cost function
+%% J vs gii
 Id(1) = 0;
 SIR(1,1) = 0;
 J(1) = 0;
 
-% J vs gii
+% J vs gii 
 i=1;
-while gii(1,i)<200
+g_upper = 200;
+while gii(1,i)<g_upper
     I = pk*gik+pj'*gij+n;
     SIR(1,i) = pi*gii(1,i)/I;
-    J(i) = (2*gii(1,i)/I).*sigmoid(I,gii(1,i))+c.*(tau-SIR(1,i))^2;
+    J(i) = (2*gii(1,i)/I).*sigmoid(I,gii(1,i))*pi+c.*(tau-SIR(1,i))^2;
     gii(1,i+1) = gii(1,i) +1;
     i = i+1;
 end
 gii = gii(1,1:i-1);
-%% plots
-figure(1)
+% plot
+figure
 plot(gii(:),J)
+xlabel('gii')
+ylabel('J')
+title('cost vs gii')
+
+
+%% J vs pi
+% resetting values
+gii = gii(1);
+J = 0;
+SIR = 0;
+I = 0;
+
+i=1;
+pi_initial = pi;
+pi_upper =0.15;
+while pi_initial(i)<pi_upper
+    I = pk*gik+pj'*gij+n;
+    SIR(i) = pi_initial(i)*gii/I;
+    J(i) = (2*gii/I).*sigmoid(I,gii)*pi_initial(i)+c.*(tau-SIR(i))^2;
+    pi_initial(i+1) = pi_initial(i) + 0.001;
+    i = i+1;
+end
+pi_initial = pi_initial(1:i-1);
+% plot
+figure
+plot(pi_initial(:),J)
+xlabel('pi')
+ylabel('J')
+title('cost vs pi')
+hold on
+plot(pi_initial(J==min(J)),min(J),'o')
+
+
+
